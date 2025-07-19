b064211398dede86baf6639bb16a3eb3e8e30c6a
Balaji ARAM VALARTHA SUNDARAM <balaji@lakshmibalajio2o.com>
2025-07-19 13:27:24 +0000

    Add Lakshmi Balaji in front of O2O on Footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
     <footer className="bg-background/80 backdrop-blur-sm shadow-inner py-4 px-4 sm:px-6 lg:px-8">
       <div className="container mx-auto text-center text-sm text-muted-foreground">
         <p>
-          <b className="font-bold text-foreground">O2O</b> | Simplifying Real Estate Transactions. All rights reserved.
+          Lakshmi Balaji <b className="font-bold text-foreground">O2O</b> | Simplifying Real Estate Transactions. All rights reserved.
         </p>
       </div>
     </footer>
